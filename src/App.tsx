b78c4bca8b08e297d1ebca8f544d91e5814b4a3d
dtjohnson83 <118701634+dtjohnson83@users.noreply.github.com>
2025-07-12 16:04:44 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-// In src/App.tsx (or wherever your main app component is)
 import AgentRegistrationSystem from './components/AgentRegistrationSystem'
 
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <AgentRegistrationSystem />
-    </div>
-  )
+export default function App() {
+  return <AgentRegistrationSystem />
 }
-
-export default App
