33fac051eabebe3d2ab1706d8fb51c67ebe757f5
dtjohnson83 <118701634+dtjohnson83@users.noreply.github.com>
2025-07-12 20:07:12 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import AgentRegistrationSystem from './components/ui/omniscope/src/components/AgentRegistrationSystem';
+import AgentRegistrationSystem from './components/AgentRegistrationSystem';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <AgentRegistrationSystem />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
