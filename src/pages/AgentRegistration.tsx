a791cad09486cd8e7a13caa2a4611604333c177c
dtjohnson83 <118701634+dtjohnson83@users.noreply.github.com>
2025-07-14 07:05:17 -0700

    Create AgentRegistration.tsx

---
+++ src/pages/AgentRegistration.tsx
@@ -0,0 +1,7 @@
+import AgentRegistrationSystem from '@/components/AgentRegistrationSystem';
+
+const AgentRegistration = () => {
+  return <AgentRegistrationSystem />;
+};
+
+export default AgentRegistration;
